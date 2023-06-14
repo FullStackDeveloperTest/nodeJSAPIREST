71162e1ab912087e52b1116e066e3eff8738bf0c
ebastidasp <ebastidasp@unal.edu.co>
2023-06-14 14:09:43 -0500

    fix: minor fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to API REST application." });
 });
 
 require("./routes/user.route")(app);
